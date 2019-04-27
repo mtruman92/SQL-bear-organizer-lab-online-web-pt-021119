a9ff81e2be1f33d90fe097911d17adb6dbb2a18a
Mickae Truman <mtruman@nyit.edu>
2019-04-27 20:34:57 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color) VALUES ('Mr. Chocolate', 15, 'Male', 'Dark Brown');
+INSERT INTO bears (name, age, gender, color) VALUES ('Rowdy', 13, 'Male', 'Tan');
+INSERT INTO bears (name, age, gender, color) VALUES ('Tabitha', 11, 'Female', 'Light Brown');
+INSERT INTO bears (name, age, gender, color) VALUES ('Sergeant Brown', 30, 'Male', 'Brown');
+INSERT INTO bears (name, age, gender, color) VALUES ('Melissa', 14, 'Female', 'Copper');
+INSERT INTO bears (name, age, gender, color) VALUES ('Grinch', 18, 'Male', 'Green Brown');
+INSERT INTO bears (name, age, gender, color) VALUES ('Wendy', 12, 'Female', 'Dark Brown');
+INSERT INTO bears (name, age, gender, color) VALUES (NULL, 22, 'Male', 'Black');
